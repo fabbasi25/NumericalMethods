175960cdc55a66297bfaf112e23202ab42a87ff6
fabbasi25 <fabbasi@mit.edu>
2026-01-22 20:31:25 -0500

    adding homework2

--- Homeworks/homework2.py
+++ Homeworks/homework2.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+import math 
 
 ## PROBLEM 1: QUANTUM MECHANICS ON THE COMPUTER 
 ## Let φ(x)= dΨ/dx. Then, dφ(x)/dx = -2m * (EΨ(x)-V(x)Ψ(x))
@@ -44,7 +45,6 @@ def rk_schr(a, b, n, x, filename, corr, m, E, V, *V_args, **V_kwargs):
 
     plt.figure(figsize=(8, 8))
 
-    print(max(psi_vals))
 
     plt.plot(x_vals, psi_vals, label="numerical $\psi(x)$")
     # plt.plot(x_vals, corr_vals, label="analytic $\psi(x)$", linestyle="--")
@@ -56,11 +56,11 @@ def rk_schr(a, b, n, x, filename, corr, m, E, V, *V_args, **V_kwargs):
     plt.tight_layout()
     plt.savefig(f'./Homeworks/{filename}', dpi=300)
 
-    return 
+    return x
 
 
 # Run RK4 for this problem 
-n = 10000 
+n = 1000
 m = 1
 
 def test_V1(x): return 0 
@@ -74,22 +74,122 @@ x1_init = [0, 1]
 
 E = 10
 # normalization constant 0.22360679699168837
-# rk_schr(0, 10, n, x1_init, "free_system", corr1, m, E, test_V1)
+rk_schr(0, 10, n, x1_init, "free_system", corr1, m, E, test_V1)
 E = (np.pi**2)/200
 # normalization constant 3.183098861837903
-# rk_schr(0, 10, n, x1_init, "free_system_E2", corr1, m, E, test_V1)
+rk_schr(0, 10, n, x1_init, "free_system_E2", corr1, m, E, test_V1)
 
 w = 1 
 E0 = w/2
 
 x2_init = [0, 1]
-# rk_schr(-5, 5, n, x2_init, "sho", corr2_gs, m, E0, test_V2, m, w)
+rk_schr(-5, 5, n, x2_init, "sho", corr2_gs, m, E0, test_V2, m, w)
 # Plotting multiples of E0 give excited states of the SHO. 
 
-# rk_schr(-5, 5, n, x2_init, "sho_3E", corr2_gs, m, 3*E0, test_V2, m, w)
-# rk_schr(-5, 5, n, x2_init, "sho_5E", corr2_gs, m, 5*E0, test_V2, m, w)
+rk_schr(-5, 5, n, x2_init, "sho_3E", corr2_gs, m, 3*E0, test_V2, m, w)
+rk_schr(-5, 5, n, x2_init, "sho_32E", corr2_gs, m, 3.5*E0, test_V2, m, w)
+
+rk_schr(-5, 5, n, x2_init, "sho_5E", corr2_gs, m, 5*E0, test_V2, m, w)
 
 
 ## PROBLEM 2: THE SHOOTING METHOD 
+def rk_isw(E, a, b, n, x, m, V, *V_args, **V_kwargs): 
+    x = np.array(x)
+    t = a 
+    h = (b-a)/n
+
+    for i in range(n): 
+        t += h
+        xt = runge_kutta(sch_rhs, t, h, x, m, E, V, *V_args, **V_kwargs)
+        x = xt 
+
+    return x[0]
+
+w = 1 
+E_vals = np.linspace(0, 3*w, 1000)
+psi_vals = []
+
+for E in E_vals: 
+    psi_vals.append(rk_isw(E, -5, 5, n, [0, 1], m, test_V2, m, w))
+plt.figure(figsize=(8, 8))
+
+
+plt.plot(E_vals, psi_vals, label="numerical $\psi(x)$")
+
+plt.xlabel('E', fontsize=12)
+plt.ylabel('$\psi(x)$', fontsize=12)
+plt.legend(fontsize=10)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig(f'./Homeworks/inf_sq', dpi=300)
+
+# bisection 
+
+def root_finder(Emin, Emax, f, *f_args, threshold=1e-5, **fkwargs): 
+    x0 = Emin   
+    roots = [] 
+    while x0 <= Emax:
+        a = x0 
+        b = a + 0.01 
+
+        if f(a, *f_args, **fkwargs)*f(b, *f_args, **fkwargs) < 0: 
+            while abs(a-b) > threshold:
+                mid = (a+b)/2
+                fmid = f(mid, *f_args, **fkwargs)
+                if fmid == 0: 
+                    break
+                if f(a, *f_args, **fkwargs)*fmid < 0: 
+                    b = mid 
+                elif f(b, *f_args, **fkwargs)*fmid < 0:
+                    a = mid 
+
+            roots.append(mid)
+        
+        x0 += 0.01 
+
+    return roots 
+
+# shooting method for free system 
+E_min = 0
+E_max = 1.5
+
+i_values = [i for i in range(5)]
+correct = [(np.pi**2)/(200)*(i+1)**2 for i in range(5)]
+E_roots = root_finder(E_min, E_max, rk_isw, 0, 10, n, [0, 1], m, test_V1, threshold=1e-10)
+
+
+plt.figure(figsize=(8, 8))
+
+plt.plot(i_values, E_roots, label="numerical estimates", marker="o")
+plt.plot(i_values, correct, label="predictions", marker="*")
+
+plt.xlabel('n', fontsize=12)
+plt.ylabel('$E_n$', fontsize=12)
+plt.legend(fontsize=10)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig(f'./Homeworks/shooting_zero_free', dpi=300)
+
+    
+# shooting method for harmonic oscillator 
+
+E_min = 0
+E_max = 5
+i_values = [i for i in range(5)]
+correct = [w/2*(2*i+1) for i in range(5)]
+E_roots = root_finder(E_min, E_max, rk_isw, -5, 5, n, [0, 1], m, test_V2, m, w, threshold=1e-10)
+
+plt.figure(figsize=(8, 8))
+
+plt.plot(i_values, E_roots, label="numerical estimates", marker="o")
+plt.plot(i_values, correct, label="predictions", marker="*", linestyle="--")
+
+plt.xlabel('n', fontsize=12)
+plt.ylabel('$E_n$', fontsize=12)
+plt.legend(fontsize=10)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig(f'./Homeworks/shooting_zero_sho', dpi=300)
+
 
 
